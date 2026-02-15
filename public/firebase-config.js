3abd71a04bb8449106ea78211e8b40d06c84ae45
Security Remediation <security@remediation.local>
2026-02-15 18:06:40 +0700

    Security: Refactor secrets to config files

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,15 +2,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "***REMOVED***",
-    authDomain: "money-manager-pro-4930f.firebaseapp.com",
-    projectId: "money-manager-pro-4930f",
-    storageBucket: "money-manager-pro-4930f.firebasestorage.app",
-    messagingSenderId: "644015644624",
-    appId: "1:644015644624:web:a28fbcc1d70f605021a3d5",
-    measurementId: "G-9CY21SJ5S0"
-};
+import { firebaseConfig } from "./config.js";
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
